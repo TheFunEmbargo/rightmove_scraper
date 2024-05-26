d551f925c5d457420bbbb7aa819a9cd0526fe6ca
Sam Roberts <TheFunEmbargo@gmail.com>
2024-05-26 14:54:11 +0100

    SCR-12 check for environment variables set

--- src/config.py
+++ src/config.py
@@ -3,5 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CITY_CENTRE = os.getenv("CITY_CENTRE")
-MAX_DISTANCE_FROM_CENTER_MILES = os.getenv("MAX_DISTANCE_FROM_CENTER_MILES")
+try:
+    CITY_CENTRE = eval(os.getenv("CITY_CENTRE"))
+    MAX_DISTANCE_FROM_CENTER_MILES = float(os.getenv("MAX_DISTANCE_FROM_CENTER_MILES"))
+except Exception:
+    raise EnvironmentError(
+        "Please set valid values (see .env.example) for enviornment variables CITY_CENTRE, MAX_DISTANCE_FROM_CENTER_MILES"
+    )
